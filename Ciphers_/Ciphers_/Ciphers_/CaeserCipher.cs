81860176153f5dd3ece9ecadfb300c6c79ce4932
ArQadeZa <70601167+ArQadeZa@users.noreply.github.com>
2020-11-25 15:36:43 +0000

    Add files via upload

---
+++ Ciphers_/Ciphers_/Ciphers_/CaeserCipher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ciphers_
+{
+    class CaeserCipher
+    {
+        //Caeser Cipher works with substituting one letter with another]
+
+        //properties
+        public string output { get; set; }
+        //methods
+        public string encipher(string input, int key)
+        {
+            this.output = ""; //clears the output
+
+            foreach (char letter in input)
+            {
+                this.output += cipher(letter, key); //runs every item through the cipher method to be encrypted 
+                                                    //and then adds them to the output property as a string
+            }
+            return this.output;
+        }
+
+        private char cipher(char letter, int key)
+        {
+            if (char.IsLetter(letter) != true)
+            {
+                return letter; //if the letter is not a alphabetical letter it gets sent back and doesnt get encrypted
+            }
+            char d = char.IsUpper(letter) ? 'A' : 'a'; //if it is upper it gets reffered to a capital encryption if its not it gets referred to a lowecase encryption
+            return (char)((((letter + key) - d) % 26) + d); //%26 gets the remainder that has to be added and then adds it starting from the beginning
+
+        }
+
+        public string decrypt(string input, int key) //input in this instance is the encrypted sentence
+        {
+            return encipher(input, 26 - key);
+        }
+    }
+}
+
---
+++ Ciphers_/Ciphers_/Ciphers_/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ciphers_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCipher_Click(object sender, RoutedEventArgs e)
+        {
+                //get values from textboxes
+                Random random = new Random();//to generate a random number for every key
+
+                string message = txtUserInput.Text;
+                int key = random.Next(1, 26);// generates a random number between 1 and 26
+
+                //call the caeser cypher class
+                CaeserCipher caeserCipher = new CaeserCipher();
+
+                //give variables values from the methods
+                string encrypted_message = caeserCipher.encipher(message, key);
+               
+                //Display the values
+                txtCipherText.Text = "Key: " + key;
+                txtCipherText.Text += "\nEncrypted Message: " + encrypted_message;
+
+            
+            
+        }
+
+        
+        private void btnDeCipher_Click(object sender, RoutedEventArgs e)
+        {
+                //assign variables to the textboxes
+                string encryptedText = txtEncryptedText.Text;
+                int key = Convert.ToInt32(txtKey.Text);
+
+                //call the caeser cipher class
+                CaeserCipher caeserCipher = new CaeserCipher();
+
+                //use the decrypt method 
+                txtDeCipherText.Text = "Decrypted Message: "+caeserCipher.decrypt(encryptedText,key);
+            
+        }
+    }
+}
